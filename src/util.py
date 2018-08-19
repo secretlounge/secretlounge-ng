f1f4db7a5843d9fb0713c269027b62522de40b5a
sfan5 <sfan5@live.de>
2018-08-19 20:00:57 +0200

    Fix salt generation for some tripcodes

--- src/util.py
+++ src/util.py
@@ -80,6 +80,9 @@ def genTripcode(tripcode):
 	trpass = tripcode[pos+1:]
 
 	salt = (trpass[:8] + 'H.')[1:3]
+	salt_chars = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	salt = "".join(c if c in salt_chars else "." for c in salt)
+
 	trip_final = crypt(trpass[:8], salt)
 
 	return trname + " !" + trip_final[-10:]
