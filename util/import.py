c9cd48656a91ae032c07abdcb5114b76a56c311a
Solar Bear <97406365+solarbearcub@users.noreply.github.com>
2023-04-07 01:47:06 +0100

    Revert changes to import

--- util/import.py
+++ util/import.py
@@ -50,7 +50,6 @@ def main(configpath, importpath):
 			u.warnExpiry = safe_time(j["warnUpdated"] // 1000) + timedelta(hours=WARN_EXPIRE_HOURS)
 		u.karma = j.get("karma", 0)
 		u.hideKarma = j.get("hideKarma", False)
-		u.hideRequests = j.get("hideRequests", False)
 		u.debugEnabled = j.get("debug", False)
 
 		if u.id in had_ids:
